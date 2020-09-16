1529f41ca8ebf6b89e5e69c3611c9368c52f80a4
zeeq0h <62445442+zeeq0h@users.noreply.github.com>
2020-09-16 15:01:44 +0100

    Update movies class.py

--- Python Course/Section 3/movies class.py
+++ Python Course/Section 3/movies class.py
@@ -1,15 +1,11 @@
 # Define a Movie class that has two properties: name and director
-
-
-# You should be able to create Movie objects like this one:
-
-
-# =============================
-
 class Movies:
     def __init__(self, new_title, new_director):
         self.title = new_title
         self.director = new_director
+
+    def print_info(self):
+        print(f"<<{self.title}>> by {self.director}")
 
 movie_one = Movies("The Matrix", "Wachowski")
 movie_two = Movies("Inception", "Christopher Nolan")
